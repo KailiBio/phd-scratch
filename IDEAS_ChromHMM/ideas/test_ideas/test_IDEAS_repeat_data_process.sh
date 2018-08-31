e178714c10b9416442319a000e4c8eecd2936fdc
KailiBio <fankaili.bio@gmail.com>
2018-08-31 09:36:41 -0400

    seperate codes for running

--- IDEAS_ChromHMM/ideas/test_ideas/test_IDEAS_repeat_data_process.sh
+++ IDEAS_ChromHMM/ideas/test_ideas/test_IDEAS_repeat_data_process.sh
@@ -11,14 +11,14 @@ cd ${workDir}
 # 1. get file from json and metadata
 python ${scriptDir}get_ideas_input_hm_bam.py
 #
-awk '{FS=OFS="\t"}{if(NR<451){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_1.sh
-awk '{FS=OFS="\t"}{if(NR>(1*450) && NR<(2*450+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_2.sh
-awk '{FS=OFS="\t"}{if(NR>(2*450) && NR<(3*450+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_3.sh
-awk '{FS=OFS="\t"}{if(NR>(3*450) && NR<(4*450+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_4.sh
-awk '{FS=OFS="\t"}{if(NR>(4*450) && NR<(5*450+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_5.sh
-awk '{FS=OFS="\t"}{if(NR>(5*450) && NR<(6*450+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_6.sh
-awk '{FS=OFS="\t"}{if(NR>(6*450) && NR<(7*450+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_7.sh
-awk '{FS=OFS="\t"}{if(NR>(7*450) && NR<(8*450+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_8.sh
+awk '{FS=OFS="\t"}{if(NR<450){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_1.sh
+awk '{FS=OFS="\t"}{if(NR>(1*450-1) && NR<(2*450+1-2)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_2.sh
+awk '{FS=OFS="\t"}{if(NR>(2*450-2) && NR<(3*450+1+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_3.sh
+awk '{FS=OFS="\t"}{if(NR>(3*450-1+2) && NR<(4*450+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_4.sh
+awk '{FS=OFS="\t"}{if(NR>(4*450) && NR<(5*450+1-1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_5.sh
+awk '{FS=OFS="\t"}{if(NR>(5*450-1) && NR<(6*450+1-2)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_6.sh
+awk '{FS=OFS="\t"}{if(NR>(6*450-2) && NR<(7*450+1+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_7.sh
+awk '{FS=OFS="\t"}{if(NR>(7*450-1+2) && NR<(8*450+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_8.sh
 awk '{FS=OFS="\t"}{if(NR>(8*450) && NR<(9*450+1)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_9.sh
 awk '{FS=OFS="\t"}{if(NR>(9*450)){print $0}}' codes_for_getting_signal_bam.sh > ./code/codes_for_getting_signal_bam_10.sh
 #
