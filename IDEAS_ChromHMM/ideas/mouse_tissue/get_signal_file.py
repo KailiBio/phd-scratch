3bfa50703c44cfd48c2b24726ea615b27e66f9fd
KailiBio <fankaili.bio@gmail.com>
2018-09-27 19:52:07 -0400

    get pvalue from ENCODE

--- IDEAS_ChromHMM/ideas/mouse_tissue/get_signal_file.py
+++ IDEAS_ChromHMM/ideas/mouse_tissue/get_signal_file.py
@@ -21,7 +21,7 @@ def write_file(file, output_path):
 if __name__ == "__main__":
     url = "https://www.encodeproject.org/search/?type=Experiment&assay_title=ChIP-seq&assay_title=WGBS&assay_title=DNase-seq&assay_title=ATAC-seq&replicates.library.biosample.donor.organism.scientific_name=Mus+musculus&biosample_type=tissue&assembly=mm10&organ_slims=brain&organ_slims=embryo&organ_slims=heart&organ_slims=liver&organ_slims=limb&organ_slims=intestine&organ_slims=kidney&organ_slims=lung&organ_slims=stomach&limit=all&format=json"
     outDir = "/data/zusers/fankaili/ideas/mm10_tissue_used_list.txt"
-    codeDir = "/data/zusers/fankaili/ideas/get_macs2_pvalue_signal_code.sh"
+    codeDir = "/data/zusers/fankaili/ideas/code/get_macs2_pvalue_code/get_macs2_pvalue_signal_code.sh"
 
     tissue_list = ["forebrain", "midbrain", "hindbrain", "neural tube", "heart", "embryonic facial prominence", "limb", "kidney", "lung", "stomach", "intestine", "liver"]
 
@@ -48,13 +48,11 @@ if __name__ == "__main__":
                 elif f.output_type=="signal p-value" and f.file_format=="bigWig" and f.bio_rep==[1] and f.tech_rep==['1_1'] and f.assembly=="mm10":
                     fileID = f.accession
             # code line for getting signal
-            code.append("cp /data/projects/encode/data/"+expID+"/"+fileID+".bw > /tmp/"+expID+"/"+fileID+".bw"+"\n")
-            code.append("bigWigAverageOverBed /tmp/"+expID+"/"+fileID+".bw /data/zusers/fankaili/ideas/run_ideas_p_value/mm10_tab.bed /data/zusers/fankaili/ideas/signal/macs2_pvalue/"+sample+"_"+mark+"_macs2_pvalue.tab"+"\n")
+            code.append("bigWigAverageOverBed /data/projects/encode/data/"+expID+"/"+fileID+".bw /data/zusers/fankaili/ideas/run_ideas_p_value/mm10_tab.bed /data/zusers/fankaili/ideas/signal/macs2_pvalue/"+sample+"_"+mark+"_macs2_pvalue.tab"+"\n")
             if mark=="WGBS":
                 code.append("cut -f 6 /data/zusers/fankaili/ideas/signal/macs2_pvalue/"+sample+"_"+mark+"_macs2_pvalue.tab > /data/zusers/fankaili/ideas/signal/macs2_pvalue/"+sample+"_"+mark+"_macs2_pvalue.txt"+"\n")
             else:
                 code.append("cut -f 5 /data/zusers/fankaili/ideas/signal/macs2_pvalue/"+sample+"_"+mark+"_macs2_pvalue.tab > /data/zusers/fankaili/ideas/signal/macs2_pvalue/"+sample+"_"+mark+"_macs2_pvalue.txt"+"\n")
-            code.append("rm /tmp/"+expID+"/"+fileID+".bw"+"\n")
             # input
             filePath="/data/zusers/fankaili/ideas/signal/macs2_pvalue/"+sample+"_"+mark+"_macs2_pvalue.txt"
             out.append(("\t").join([sample, mark, filePath, expID, fileID])+"\n")
