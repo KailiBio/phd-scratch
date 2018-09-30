fd1dbd2b9ce31757fafb1795e36c6df36b61e0b1
KailiBio <fankaili.bio@gmail.com>
2018-09-30 19:11:45 -0400

    remove duplicate expressional file

---
+++ bimodal_TF_in_ubi-rDHS/hg38_ubi-rDHS_with_gene_regulation/get_GRCh38_tissue_RAMPAGE_exp.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# -- Kaili
+# This script is for getting GRCh38 RNA-seq exp data in all 113 tissues from ENCODE tsv file.
+
+
+import re, os, sys
+import subprocess
+
+sys.path.append('/home/fankaili/git/metadata/utils')
+from querydcc import QueryDCC
+from exp import Exp
+from exp_file import ExpFile
+
+def write_file(file, output_path):
+	o= open(output_path, 'w')
+	o.writelines(file)
+	o.close()
+
+##################################
+
+if __name__ == "__main__":
+    url = "https://www.encodeproject.org/search/?type=Experiment&assay_title=RAMPAGE&replicates.library.biosample.donor.organism.scientific_name=Homo+sapiens&assembly=GRCh38&lab.title=Thomas+Gingeras%2C+CSHL&biosample_type=tissue&limit=all&format=json"
+    outDir = "/data/zusers/fankaili/ccre/hg38_ubi-rDHS/hg38_tissue_TSS_exp_list.txt"
+
+    q = QueryDCC(auth=False)
+    out = []
+    for exp in q.getExps(url):
+	sample = ("_").join([exp.biosample_term_name.replace(" ","_"), exp.age_display.replace(" ","_")])
+	expID = exp.files[0].expID
+        for f in exp.files:
+            if f.bio_rep==[1] and f.tech_rep==['1_1'] and f.file_format=="bigWig" and f.output_type=="plus strand signal of unique reads" and f.assembly=="GRCh38":
+		plus = f.accession
+		print(plus)
+		# subprocess.call("bigWigAverageOverBed /data/projects/encode/data/"+expID+"/"+plus+".bigWig /data/zusers/fankaili/ccre/hg38_ubi-rDHS/TSS.Filtered_sorted.bed /data/zusers/fankaili/ccre/hg38_ubi-rDHS/tissue_rampage/"+plus+".tab", shell=True)
+	    elif f.bio_rep==[1] and f.tech_rep==['1_1'] and f.file_format=="bigWig" and f.output_type=="minus strand signal of unique reads" and f.assembly=="GRCh38":
+		minus = f.accession
+	        print(minus)
+		# subprocess.call("bigWigAverageOverBed /data/projects/encode/data/"+f.expID+"/"+minus+".bigWig /data/zusers/fankaili/ccre/hg38_ubi-rDHS/TSS.Filtered_sorted.bed /data/zusers/fankaili/ccre/hg38_ubi-rDHS/tissue_rampage/"+minus+".tab", shell=True)
+	out.append(("\t").join([expID, plus, minus, sample])+"\n")
+    write_file(out,outDir)
