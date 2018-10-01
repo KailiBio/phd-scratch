aa6bdf83440ceaa6d6f99d302c515b7020ee3769
KailiBio <fankaili.bio@gmail.com>
2018-09-30 22:16:21 -0400

    add TSS index hist

---
+++ bimodal_TF_in_ubi-rDHS/hg38_ubi-rDHS_with_gene_regulation/find_TSscore_cutoff.R
@@ -0,0 +1,69 @@
+
+# -- Kaili
+# This script is for plotting tissue specificity index and finding the cut-off.
+
+setwd("/Users/kaili/Dropbox (UMass Medical School)/Project/ccre/hg38_rOCR/")
+
+calculate_TSindex = function(x){
+  s=0
+  if(max(x)==0){return(NA)}
+  else{
+    for(i in x){
+      s=s+((max(x)-i)/max(x))
+    }
+    return(s/(length(x)-1))
+  }
+}
+
+######################
+# Xiaoou's data
+######################
+data = read.table("all_ts.txt", header = TRUE, row.names = 1)
+pdf("mouse_ts_score_xiaoou.pdf")
+hist(data[,1], breaks=200, xlab = "tissue specificity index", 
+     main = "Xiaoou's mouse data\n(66 samples)", freq = FALSE)
+lines(c(0.52,0.52), c(0,400), col="red", lwd=2, lty=2)
+dev.off()
+
+######################
+# 
+######################
+ts_score = read.table("hg38_tissue_gene_exp_TSscore.txt", header = TRUE, row.names = 1)
+exp = read.table("hg38_tissue_gene_exp_matrix.txt", header = TRUE, row.names = 1)
+hk = as.vector(read.table("hg38_housekeeping_geneID_geneType_geneSymbol.txt")[,1])
+
+matrix = transform(ts_score, type="nonHK")
+matrix$type = as.vector(matrix$type)
+matrix[hk,]$type = "HK_published"
+
+library(ggplot2)
+library(gridExtra)
+pdf("tissue_specificity_index.pdf", width=12, height = 6)
+g1 = ggplot(matrix, aes(all, fill = type)) + 
+  geom_histogram(alpha = 0.5, aes(y = ..density..), position = 'identity') +
+  xlab("tissue_specificity_index")
+g2 = ggplot(matrix, aes(all, fill = type)) + 
+  geom_histogram(alpha = 0.5, aes(y = ..count..), position = 'identity') +
+  xlab("tissue_specificity_index")
+grid.arrange(g1, g2, ncol=2)
+dev.off()
+
+######################
+
+# library(preprocessCore)
+# exp_q = normalize.quantiles(as.matrix(exp), copy = TRUE)
+# ts_score_q=apply(exp_q,1,fun_tes)
+# hist(ts_score_q, breaks=100)
+
+
+######################
+# TSS ts index
+######################
+tss_ts = read.table("hg38_tissue_TSS_exp_TSscore.txt", header = TRUE, row.names = 1)
+
+ggplot(tss_ts, aes(all)) + 
+  geom_histogram(alpha = 0.5, aes(y = ..count..), position = 'identity') + 
+  xlab("tissue_specificity_index")
+ggsave("TSS_tissue_specificity_index.pdf")
+
+
