43f4b993c35bdeaa051e4ab01f238ad487d9b449
KailiBio <fankaili.bio@gmail.com>
2018-11-14 19:55:11 -0500

    run dhs-bins v3

--- IDEAS_ChromHMM/ideas/DHS_bins/2_get_signal.sh
+++ IDEAS_ChromHMM/ideas/DHS_bins/2_get_signal.sh
@@ -4,6 +4,7 @@
 # This script is for getting MACS2 p-value signal for given DHS-center bins file.
 # 1. v1_100_300bp
 # 2. v2_1_300bp
+# 3. v3_150_350bp
 
 scriptDir="/data/zusers/fankaili/github/weng-lab/Kaili/IDEAS_ChromHMM/ideas/DHS_bins/"
 workDir="/data/zusers/fankaili/ideas/dhs_bins/"
@@ -106,3 +107,58 @@ vim DHS_v2_1-300bp.parafile
 
 # run IDEAS
 nohup bash DHS_v2_1-300bp.sh > ./nohup.DHS_v2_1-300bp.out 2>&1&
+
+
+
+
+# 3. v3_150_350bp
+sort -k1,1 mm10_OCR-center_bins_v3.bed > mm10_OCR-center_bins_v3.sorted.bed
+python ${scriptDir}get_66samples_10marks_signal_pvalue.py /data/zusers/fankaili/ideas/dhs_bins/mm10_OCR-center_bins_v3.sorted.bed \
+/data/zusers/fankaili/ideas/dhs_bins/v3_150_350bp/
+#
+cd ${workDir}v3_150_350bp/
+mkdir signal
+mkdir code
+# divide code file
+awk '{FS=OFS="\t"}{if(NR<133){print $0}}' codes_for_getting_signal_pvalue.sh > ./code/codes_for_getting_signal_pvalue_1.sh
+awk '{FS=OFS="\t"}{if(NR>(1*132) && NR<(2*132+1)){print $0}}' codes_for_getting_signal_pvalue.sh > ./code/codes_for_getting_signal_pvalue_2.sh
+awk '{FS=OFS="\t"}{if(NR>(2*132) && NR<(3*132+1)){print $0}}' codes_for_getting_signal_pvalue.sh > ./code/codes_for_getting_signal_pvalue_3.sh
+awk '{FS=OFS="\t"}{if(NR>(3*132) && NR<(4*132+1)){print $0}}' codes_for_getting_signal_pvalue.sh > ./code/codes_for_getting_signal_pvalue_4.sh
+awk '{FS=OFS="\t"}{if(NR>(4*132) && NR<(5*132+1)){print $0}}' codes_for_getting_signal_pvalue.sh > ./code/codes_for_getting_signal_pvalue_5.sh
+awk '{FS=OFS="\t"}{if(NR>(5*132) && NR<(6*132+1)){print $0}}' codes_for_getting_signal_pvalue.sh > ./code/codes_for_getting_signal_pvalue_6.sh
+awk '{FS=OFS="\t"}{if(NR>(6*132) && NR<(7*132+1)){print $0}}' codes_for_getting_signal_pvalue.sh > ./code/codes_for_getting_signal_pvalue_7.sh
+awk '{FS=OFS="\t"}{if(NR>(7*132) && NR<(8*132+1)){print $0}}' codes_for_getting_signal_pvalue.sh > ./code/codes_for_getting_signal_pvalue_8.sh
+awk '{FS=OFS="\t"}{if(NR>(8*132) && NR<(9*132+1)){print $0}}' codes_for_getting_signal_pvalue.sh > ./code/codes_for_getting_signal_pvalue_9.sh
+awk '{FS=OFS="\t"}{if(NR>(9*132) && NR<(10*132+1)){print $0}}' codes_for_getting_signal_pvalue.sh > ./code/codes_for_getting_signal_pvalue_10.sh
+# run code to get signal file
+nohup bash ./code/codes_for_getting_signal_pvalue_1.sh > ./code/nohup.codes_for_getting_signal_pvalue_1.out 2>&1&
+nohup bash ./code/codes_for_getting_signal_pvalue_2.sh > ./code/nohup.codes_for_getting_signal_pvalue_2.out 2>&1&
+nohup bash ./code/codes_for_getting_signal_pvalue_3.sh > ./code/nohup.codes_for_getting_signal_pvalue_3.out 2>&1&
+nohup bash ./code/codes_for_getting_signal_pvalue_4.sh > ./code/nohup.codes_for_getting_signal_pvalue_4.out 2>&1&
+nohup bash ./code/codes_for_getting_signal_pvalue_5.sh > ./code/nohup.codes_for_getting_signal_pvalue_5.out 2>&1&
+nohup bash ./code/codes_for_getting_signal_pvalue_6.sh > ./code/nohup.codes_for_getting_signal_pvalue_6.out 2>&1&
+nohup bash ./code/codes_for_getting_signal_pvalue_7.sh > ./code/nohup.codes_for_getting_signal_pvalue_7.out 2>&1&
+nohup bash ./code/codes_for_getting_signal_pvalue_8.sh > ./code/nohup.codes_for_getting_signal_pvalue_8.out 2>&1&
+nohup bash ./code/codes_for_getting_signal_pvalue_9.sh > ./code/nohup.codes_for_getting_signal_pvalue_9.out 2>&1&
+nohup bash ./code/codes_for_getting_signal_pvalue_10.sh > ./code/nohup.codes_for_getting_signal_pvalue_10.out 2>&1&
+
+# .bed file
+awk '{FS="\t";OFS=" "}{print $1,$2,$3,$4}' ${workDir}mm10_OCR-center_bins_v3.sorted.bed > mm10_OCR-center_bins_v3.sorted_space.bed
+awk '{FS="\t";OFS=" "}{if(NR==FNR){a[$4]=$0}else{print a[$1]}}' /data/zusers/fankaili/ideas/dhs_bins/mm10_OCR-center_bins_v3.bed \
+/data/zusers/fankaili/ideas/dhs_bins/v3_150_350bp/signal/forebrain_11.5_ATAC.tab > mm10_OCR-center_bins_v3_signal_based.bed
+awk '{FS="\t";OFS=" "}{print $1,$2,$3,$4}' mm10_OCR-center_bins_v3_signal_based.bed > mm10_OCR-center_bins_v3_signal_based_space.bed
+# .input file
+cp signal_pvalue.input DHS_v3_150-350bp.input
+# .sh file
+cp /data/zusers/fankaili/ideas/run_ideas_p_value/run_IDEAS_8hm_atac_dname_pvalue.sh DHS_v3_150-350bp.sh
+vim DHS_v3_150-350bp.sh
+# .parafile file
+cp /data/zusers/fankaili/ideas/run_ideas_p_value/run_IDEAS_8hm_atac_dname_pvalue.parafile DHS_v3_150-350bp.parafile
+vim DHS_v3_150-350bp.parafile
+
+# ./bin ./data folder
+cp -r /data/zusers/fankaili/ideas/dhs_bins/v1_100_300bp/bin ./
+cp -r /data/zusers/fankaili/ideas/dhs_bins/v1_100_300bp/data ./
+
+# run IDEAS
+nohup bash DHS_v3_150-350bp.sh > ./nohup.DHS_v3_150-350bp.out 2>&1&
