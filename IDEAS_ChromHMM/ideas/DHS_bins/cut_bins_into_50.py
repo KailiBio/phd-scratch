329330626e4bde34b540343b84dcba071e546128
KailiBio <fankaili.bio@gmail.com>
2018-11-28 22:02:05 -0500

    separate windows

--- IDEAS_ChromHMM/ideas/DHS_bins/cut_bins_into_50.py
+++ IDEAS_ChromHMM/ideas/DHS_bins/cut_bins_into_50.py
@@ -2,6 +2,16 @@
 
 # -- Kaili
 # This script is for cutting bins into 50 small windows for making aggregation plot.
+# INPUT: bed file
+#        output file for small windows in upsteam 500bp
+#        output file for small windows in bins given
+#        output file for small windows in downstream 500bp
+# OUTPUT: 3 small windows bed files
+# EXP: python cut_bins_into_50.py \
+#       /data/zusers/fankaili/ideas/dhs_bins/v3_100_400bp/mm10_OCR-center_bins_v3_signal_based.bed \
+#       /data/zusers/fankaili/ideas/dhs_bins/v3_100_400bp/mm10_OCR-center_bins_v3_50windows_bins.bed \
+#       /data/zusers/fankaili/ideas/dhs_bins/v3_100_400bp/mm10_OCR-center_bins_v3_50windows_up.bed \
+#       /data/zusers/fankaili/ideas/dhs_bins/v3_100_400bp/mm10_OCR-center_bins_v3_50windows_down.bed
 
 import re, os, sys
 import subprocess
@@ -23,20 +33,27 @@ def cut_bins_into_50(chr, start, end, prefix, output, m, n=50):
 
 
 if __name__ == "__main__":
-    bins = "/data/zusers/fankaili/ideas/dhs_bins/v3_100_400bp/mm10_OCR-center_bins_v3_signal_based.bed"
-    out_file = "/data/zusers/fankaili/ideas/dhs_bins/v3_100_400bp/mm10_OCR-center_bins_v3_50windows.bed"
 
-    # bins = "/data/zusers/fankaili/ideas/run_ideas_p_value/mm10_tab.bed"
-    # out_file = "/data/zusers/fankaili/ideas/dhs_bins/v3_100_400bp/mm10_normal_bins_50windows.bed"
+    bins = sys.argv[1]
+    bins_out_file = sys.argv[2]
+    upstream_out_file = sys.argv[3]
+    downstream_out_file = sys.argv[4]
 
-    output = open(out_file, "w+")
+    output1 = open(upstream_out_file, "w+")
+    output2 = open(bins_out_file, "w+")
+    output3 = open(downstream_out_file, "w+")
     for line in open(bins).readlines():
         chr, start, end, prefix = line.rstrip().split("\t")
         start = int(start)
         end = int(end)
         #
-        cut_bins_into_50(chr, start-500, start, prefix, output, 0, 50)
-        cut_bins_into_50(chr, start, end, prefix, output, 50, 50)
-        cut_bins_into_50(chr, end, end+500, prefix, output, 100, 50)
+        if start-500>=0:
+            cut_bins_into_50(chr, start-500, start, prefix, output1, 0, 50)
+        else:
+            cut_bins_into_50(chr, 0, start, prefix, output1, 0, 50)
+        cut_bins_into_50(chr, start, end, prefix, output2, 50, 50)
+        cut_bins_into_50(chr, end, end+500, prefix, output3, 100, 50)
 
-    output.close()
+    output1.close()
+    output2.close()
+    output3.close()
