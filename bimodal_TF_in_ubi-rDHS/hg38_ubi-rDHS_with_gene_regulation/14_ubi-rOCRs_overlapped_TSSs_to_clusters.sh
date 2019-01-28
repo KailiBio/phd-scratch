a77d0527594368bedf88f8bf22c306765fb314b1
KailiBio <fankaili.bio@gmail.com>
2019-01-28 08:51:30 -0500

    ubi-rOCRs TSS tend to cluster

--- bimodal_TF_in_ubi-rDHS/hg38_ubi-rDHS_with_gene_regulation/14_ubi-rOCRs_overlapped_TSSs_to_clusters.sh
+++ bimodal_TF_in_ubi-rDHS/hg38_ubi-rDHS_with_gene_regulation/14_ubi-rOCRs_overlapped_TSSs_to_clusters.sh
@@ -8,6 +8,7 @@ workDir="/data/zusers/fankaili/ccre/hg38_ubi-rDHS/"
 
 cd ${workDir}
 
+#Rscript ubi-rOCRs_overlapped_TSSs_to_clusters.R
 # 1. histogram: number of TSSs/ubi-rOCRs overlapped TSSs in gene
 ## number of TSSs in each gene
 cut -f 7-8 TSS.Filtered.uniqID.bed | sort -u | cut -f 1 | sort | uniq -c | awk '{OFS="\t"}{print $2,$1}' \
@@ -26,13 +27,37 @@ awk '{FS=OFS="\t"}{if(NR==FNR){a[$1]=1}else{if(a[$1]){print $1,$2}}}' \
 awk '{FS=OFS="\t"}{if(NR==FNR){a[$1]=$2;b[$1]=1}else{if(b[$1]){print $1,$2,a[$1],a[$1]/$2}else{print $1,$2,0,0}}}' \
 GRCh38_coding-gene_ubi-rOCR_TSS_count.txt GRCh38_coding-gene_allTSS_count.txt > \
 GRCh38_coding-gene_ubi-rOCR_TSS_percentage.txt
-#Rscript ubi-rOCRs_overlapped_TSSs_to_clusters.R
+awk '{FS=OFS="\t"}{if(NR==FNR){a[$1]=$2;b[$1]=1}else{if(b[$1]){print $1,$2,a[$1],a[$1]/$2}else{print $1,$2,0,0}}}' \
+GRCh38_gene_ubi-rOCR_TSS_count.txt GRCh38_gene_allTSS_count.txt > \
+GRCh38_gene_ubi-rOCR_TSS_percentage.txt
 
+cut -f 2-3 GRCh38_coding-gene_ubi-rOCR_TSS_percentage.txt | sort | uniq -c | awk '{OFS="\t"}{print $2,$3,$1}' |
+sort -k3,3nr > GRCh38_coding-gene_ubi-rOCR_TSS_percentage_sort_count.txt
 
 # 2. histogram: number of TSSs overlapped in each rOCRs/ubi-rOCRs
 intersectBed -a GRCh38-rOCRs.bed -b TSS.uniq.bed -wa -c > GRCh38_rOCRs_TSS_count.txt
 awk '{FS=OFS="\t"}{if(NR==FNR){a[$4]=1}else{if(a[$4]){print $4,$5,"ubi-rOCRs"}else{print $4,$5,"non_ubi-rOCRs"}}}' \
 GRCh38_ubi-rOCRs_EDGEid.bed GRCh38_rOCRs_TSS_count.txt > GRCh38_rOCRs_TSS_count_annotated.txt
+## for clusters: ubi-rOCRs overlapped TSSs percentage in each gene
+cut -f 7 ./merged-TSS/GRCh38_ubi-rOCR_overlapped_merged-TSS.bed | sort | uniq -c | \
+awk '{FS=" ";OFS="\t"}{print $2,$1}' > ./closest_gene/hg38_gene_overlapped_TSS_count_merged2.txt
+awk '{FS=OFS="\t"}{if(NR==FNR){a[$1]=$2}else{if(a[$1]){print $0,a[$1]}else{print $0,0}}}' \
+./closest_gene/hg38_gene_overlapped_TSS_count_merged2.txt GRCh38_coding-gene_ubi-rOCR_TSS_percentage.txt \
+> GRCh38_coding-gene_ubi-rOCR_TSS_percentage_with_cluster.txt
+# num of TSS in a gene vs singular&cluster
+awk '{FS=OFS="\t"}{if(NR==FNR){a[$1]=$2;b[$1]=1}else{if(b[$7]){if($3==$2){print $7,$4,a[$7],"singular"}else{print $7,$4,a[$7],"cluster"}}}}' \
+GRCh38_coding-gene_ubi-rOCR_TSS_percentage.txt ./merged-TSS/GRCh38_ubi-rOCR_overlapped_merged-TSS.bed \
+> GRCh38_coding-gene_numOfTSS_overlappedTSSLength.txt
+# gene that with only one TSS overlapped ubi-rOCRs
+awk '{FS=OFS="\t"}{if(NR==FNR){if($5==1){a[$1]=1}}else{if(a[$1]){print $0}}}' \
+GRCh38_coding-gene_ubi-rOCR_TSS_percentage_with_cluster.txt GRCh38_coding-gene_numOfTSS_overlappedTSSLength.txt \
+> GRCh38_coding-gene_numOfTSS1_overlappedTSSLength.txt
+# gene that with only two TSS overlapped ubi-rOCRs
+awk '{FS=OFS="\t"}{if(NR==FNR){if($5==2){a[$1]=1}}else{if(a[$1]){print $0}}}' \
+GRCh38_coding-gene_ubi-rOCR_TSS_percentage_with_cluster.txt GRCh38_coding-gene_numOfTSS_overlappedTSSLength.txt \
+| sort -k1,1 > GRCh38_coding-gene_numOfTSS2_overlappedTSSLength.txt
+awk 'BEGIN{FS=OFS="\t";gene="";type="";num=""}{if(gene!=$1){gene=$1;num=$3;type=$4}else{if(type==$4){print gene,num,type}else{print gene,num,"half"}}}' \
+GRCh38_coding-gene_numOfTSS2_overlappedTSSLength.txt > GRCh38_coding-gene_numOfTSS2_overlappedTSSLength_new.txt
 
 # 3. number of nearby TSS for each TSS
 # expand 50bp
@@ -52,3 +77,20 @@ GRCh38_ubi-rOCR_overlapped_TSS_uniqID.txt TSS_neary_ubi-TSS_count.txt > TSS_near
 cut -f 4,9,8 TSS_neary_TSS_count_annotated.txt | sort -u | sort -k1,1 > tmp.txt
 awk '{FS=OFS="\t"}{if(NR==FNR){a[$4]=$8}else{print $1,$3,$2,a[$1]}}' TSS_neary_ubi-TSS_count_annotated.txt \
 tmp.txt > TSS_neary_TSS_ubi-TSS_count_merge.txt
+
+
+# 4. distance to nearby TSSs
+bedtools closest -a TSS.Filtered.uniq_sorted.bed -b TSS.Filtered.uniq_sorted.bed -io -d | \
+awk '{FS=OFS="\t"}{print $8,$7,$16,$15,$17}' > TSS_nearest_TSS.bed
+cut -f 1,5 TSS_nearest_TSS.bed | sort -u > tmp.txt
+awk '{FS=OFS="\t"}{if(NR==FNR){a[$1]=1}else{if(a[$1]){print $1,$2,"ubi-rOCRs_overlapped_TSS"}else{print $1,$2,"remaining_TSS"}}}' \
+GRCh38_ubi-rOCR_overlapped_TSS_uniqID.txt tmp.txt > TSS_nearest_TSS_forHist_annotated.bed
+
+
+## 5. distance to nearby TSS in the same gene
+sort -k7,7 -k2,2n TSS.Filtered.uniq.bed > TSS.Filtered.uniq_sortedByGene.bed
+awk 'BEGIN{FS=OFS="\t";gene="";tss="";end="";distance=1000000}{if(gene==""){gene=$7;tss=$8;end=$3}else{if(gene==$7){if(distance>($2-end)){print gene,tss,$2-end}else{print gene,tss,distance};tss=$8;distance=$2-end;end=$3}else{print gene,tss,distance;gene=$7;tss=$8;end=$3;distance=1000000}}}END{print gene,tss,distance}' \
+TSS.Filtered.uniq_sortedByGene.bed > TSS_nearest_TSS_distance_sameGene.bed
+awk '{FS=OFS="\t"}{if(NR==FNR){a[$1]=1}else{if(a[$2]){print $0,"ubi-rOCRs_overlapped_TSS"}else{print $0,"remaining_TSS"}}}' \
+GRCh38_ubi-rOCR_overlapped_TSS_uniqID.txt TSS_nearest_TSS_distance_sameGene.bed > \
+TSS_nearest_TSS_distance_sameGene_annotated.bed
--- bimodal_TF_in_ubi-rDHS/hg38_ubi-rDHS_with_gene_regulation/ubi-rOCRs_overlapped_TSSs_to_clusters.R
+++ bimodal_TF_in_ubi-rDHS/hg38_ubi-rDHS_with_gene_regulation/ubi-rOCRs_overlapped_TSSs_to_clusters.R
@@ -21,8 +21,6 @@ coding_gene_ubi_TSS = transform(coding_gene_ubi_TSS, type="ubi-rOCRs_overlapped_
 coding_gene_matrix = rbind(coding_gene_TSS,coding_gene_ubi_TSS)
 colnames(coding_gene_matrix) = c("coding_gene","count", "type")
 
-coding_percentage = read.table("GRCh38_coding-gene_ubi-rOCR_TSS_percentage.txt")
-colnames(coding_percentage) = c("gene", "TSS", "ubi_TSS", "percentage")
 ####################
 # num of TSS in gene
 ####################
@@ -81,8 +79,10 @@ ggsave("num_TSS_in_coding-gene_count_density.png")
 
 
 ####################
-# scatter plot: TSS in gene vs. ubi-TSS in gene
+# scatter plot: TSS in gene vs. ubi-TSS in gene (percentage)
 ####################
+coding_percentage = read.table("GRCh38_coding-gene_ubi-rOCR_TSS_percentage_with_cluster.txt")
+colnames(coding_percentage) = c("gene", "TSS", "ubi_TSS", "percentage","cluster")
 
 ggplot(coding_percentage, aes(x=TSS, y=ubi_TSS, col="#f8766d")) + 
   geom_point(alpha = 0.1) + theme_minimal() + xlim(0,70) + ylim(0,70) +
@@ -91,6 +91,128 @@ ggplot(coding_percentage, aes(x=TSS, y=ubi_TSS, col="#f8766d")) +
   geom_abline(intercept = 0, slope = 0.5, col="grey", linetype="dotdash")
 ggsave("num_TSS_in_coding-gene_count_scatter.pdf")
 ggsave("num_TSS_in_coding-gene_count_scatter.png")
+
+
+ggplot(coding_percentage, aes(x=TSS, y=percentage, group=TSS)) + 
+  geom_boxplot(width=0.7, outlier.size=0.3, fill="#f8766d", color="#bdbdbd") +
+  theme_minimal() +
+  ylab("percentage of ubi-rOCRs overlapped TSSs") + xlab("number of TSSs in a gene") +
+  labs(title="percentage of ubi-rOCRs overlapped TSSs\nin protein-coding genes") +
+  theme(title = element_text(face="bold",size=12))
+ggsave("percentatge_ubi-rOCRs_TSS_in_coding-gene_boxplot.pdf", width = 10, height=5)
+ggsave("percentatge_ubi-rOCRs_TSS_in_coding-gene_boxplot.png", width = 10, height=5)
+
+a=cbind(as.numeric(names(table(coding_percentage$TSS))),as.vector(table(coding_percentage$TSS)))
+gene_TSS_num = cbind(1:73,rep(0,73))
+gene_TSS_num[a[,1],2] = a[,2]
+gene_TSS_num = data.frame(gene_TSS_num)
+colnames(gene_TSS_num) = c("num","count")
+ggplot(gene_TSS_num, aes(x=num, y=count)) + geom_bar(stat="identity", fill="#f8766d") +
+  theme_minimal() + theme(title = element_text(face="bold", size=12)) +
+  xlab("number of TSSs in a gene") + ylab("number of genes") +
+  labs(title="number of gene with different TSSs")
+
+ggsave("number_of_gene_with_TSS_count.pdf", width = 10, height=5)
+ggsave("number_of_gene_with_TSS_count.png", width = 10, height=5)
+
+##--------------
+
+
+gene_percentage = read.table("GRCh38_gene_ubi-rOCR_TSS_percentage.txt")
+colnames(gene_percentage) = c("gene", "TSS", "ubi_TSS", "percentage")
+
+ggplot(gene_percentage, aes(x=TSS, y=percentage, group=TSS)) + 
+  geom_boxplot(width=0.5, fill="#f8766d", color="#bdbdbd", outlier.size=0.3) +
+  theme_minimal() +
+  ylab("percentage of ubi-rOCRs overlapped TSSs") + xlab("number of TSSs") +
+  labs(title="percentage of ubi-rOCRs overlapped TSSs\nin genes") +
+  theme(title = element_text(face="bold",size=12))
+ggsave("percentatge_ubi-rOCRs_TSS_in_gene_boxplot.pdf")
+ggsave("percentatge_ubi-rOCRs_TSS_in_gene_boxplot.png")
+
+##---------------
+
+coding_percentage2 = read.table("GRCh38_coding-gene_ubi-rOCR_TSS_percentage_sort_count.txt")
+colnames(coding_percentage2) = c("TSS", "ubi_TSS", "count")
+#coding_percentage3 = coding_percentage2[-c(1),]
+ggplot(coding_percentage2, aes(x=TSS, y=ubi_TSS, size=count, col=count)) +
+  geom_point(aes(size=count), alpha=0.5) + 
+  scale_color_gradient(low="#bdbdbd",high="#000000") +
+  theme_minimal() + xlim(0,70) + ylim(0,70) + 
+  geom_abline(intercept = 0, slope = 1, col="grey", linetype="dashed") +
+  geom_abline(intercept = 0, slope = 0.5, col="grey", linetype="dotdash") 
+ggsave("num_TSS_in_coding-gene_count_scatter2.pdf")
+ggsave("num_TSS_in_coding-gene_count_scatter2.png")
+  
+####################
+# TSS-clusters
+####################
+
+ggplot(coding_percentage, aes(x=TSS, y=cluster, group=TSS)) + 
+  geom_boxplot(width=0.7, outlier.size=0.3, fill="#f8766d", color="#bdbdbd") +
+  theme_minimal() +
+  ylab("number of ubi-rOCRs overlapped TSS-clusters") + xlab("number of TSSs in a gene") +
+  labs(title="number of ubi-rOCRs overlapped TSS-clusters\nin protein-coding genes") +
+  theme(title = element_text(face="bold",size=12))
+ggsave("percentatge_ubi-rOCRs_TSS-cluster_in_coding-gene_boxplot.pdf", width = 10, height=5)
+ggsave("percentatge_ubi-rOCRs_TSS-cluster_in_coding-gene_boxplot.png", width = 10, height=5)
+
+
+### ubi-rOCTRs overlapped genes, 
+overlapped_TSS_type = read.table("GRCh38_coding-gene_numOfTSS_overlappedTSSLength.txt")
+colnames(overlapped_TSS_type) = c("gene", "tss", "num_of_TSS", "type") 
+
+ggplot(overlapped_TSS_type, aes(x=type, y=num_of_TSS, fill=type)) + geom_violin(width=0.5) +
+  geom_boxplot(width=0.1, outlier.size = 0) +
+  theme_minimal() + theme(title = element_text(face="bold", size=12)) +
+  labs(title="number of TSSs of gene VS. ubi-rOCRs overlapped TSSs type",
+       subtitle="wilcox.test: p-value=3.3e-134") +
+  ylab("number of TSS in a gene")
+
+ggsave("numOfTSS_overlappedTSStype_violin.pdf")
+ggsave("numOfTSS_overlappedTSStype_violin.png")
+
+x = overlapped_TSS_type[overlapped_TSS_type$type=="cluster",]$num_of_TSS
+y = overlapped_TSS_type[overlapped_TSS_type$type=="singular",]$num_of_TSS
+wilcox.test(x,y)$p.value
+
+### protein-coding gene with only 1 TSS-cluster overlapped
+overlapped_1TSS_type = read.table("GRCh38_coding-gene_numOfTSS1_overlappedTSSLength.txt")
+colnames(overlapped_1TSS_type) = c("gene", "tss", "num_of_TSS", "type") 
+
+ggplot(overlapped_1TSS_type, aes(x=type, y=num_of_TSS, fill=type)) + geom_violin(width=0.5) +
+  geom_boxplot(width=0.1, outlier.size = 0) +
+  theme_minimal() + theme(title = element_text(face="bold", size=12)) +
+  labs(title="number of TSSs of gene VS. ubi-rOCRs overlapped TSSs type",
+       subtitle="wilcox.test: p-value=4.3e-201") +
+  ylab("number of TSS in a gene")
+
+ggsave("numOfTSS1_overlappedTSStype_violin.pdf")
+ggsave("numOfTSS1_overlappedTSStype_violin.png")
+
+x = overlapped_1TSS_type[overlapped_1TSS_type$type=="cluster",]$num_of_TSS
+y = overlapped_1TSS_type[overlapped_1TSS_type$type=="singular",]$num_of_TSS
+wilcox.test(x,y)$p.value
+
+### protein-coding gene with only 1 TSS-cluster overlapped
+overlapped_2TSS_type = read.table("GRCh38_coding-gene_numOfTSS2_overlappedTSSLength_new.txt")
+colnames(overlapped_2TSS_type) = c("gene", "num_of_TSS", "type") 
+
+ggplot(overlapped_2TSS_type, aes(x=type, y=num_of_TSS, fill=type)) + geom_violin(width=0.5) +
+  geom_boxplot(width=0.1, outlier.size = 0) +
+  theme_minimal() + theme(title = element_text(face="bold", size=12)) +
+  labs(title="number of TSSs of gene VS. ubi-rOCRs overlapped TSSs type",
+       subtitle="wilcox.test:\ncluster&half p-value=2.8e-18\nhalf&singular p-value=8.5e-50") +
+  ylab("number of TSS in a gene")
+
+ggsave("numOfTSS2_overlappedTSStype_violin.pdf")
+ggsave("numOfTSS2_overlappedTSStype_violin.png")
+
+x = overlapped_2TSS_type[overlapped_2TSS_type$type=="cluster",]$num_of_TSS
+y = overlapped_2TSS_type[overlapped_2TSS_type$type=="half",]$num_of_TSS
+z = overlapped_2TSS_type[overlapped_2TSS_type$type=="singular",]$num_of_TSS
+wilcox.test(x,y)$p.value
+wilcox.test(y,z)$p.value
 
 ####################
 # boxplot:TSS nearby TSS numbers
@@ -156,11 +278,100 @@ ggplot(ocr_count, aes(count, fill=type)) +
 ggsave("num_TSS_in_rOCRs_density.pdf")
 ggsave("num_TSS_in_rOCRs_density.png")
 
+# ggplot(ocr_count, aes(count, fill=type, col=type)) +
+#   geom_density(alpha=0.5, n=26) +
+#   coord_cartesian(ylim = c(0, 5))
+
 # hist(ocr_count[ocr_count$type=="non_ubi-rOCRs",]$count, breaks=25,freq = F,col="blue")
 # hist(ocr_count[ocr_count$type=="ubi-rOCRs",]$count, breaks=26,freq = F, col="red", add=TRUE)
+
 ####################
+# histogram: TSS nearest TSS distance
 ####################
+distance = read.table("TSS_nearest_TSS_forHist_annotated.bed")
+colnames(distance) = c("tss","distance", "type")
+
+
+ggplot(distance, aes(log10(distance+0.1), fill=type, col=type)) + 
+  geom_density(size=1, alpha=0.5) +
+  scale_fill_manual(values = c("#00bfc4","#f8766d")) +
+  scale_color_manual(values = c("#00bfc4","#f8766d")) +
+  theme_minimal() +
+  geom_vline(xintercept = log10(50), col="grey",linetype="dashed", size=1) +
+  theme(title = element_text(face="bold",size=12),
+        legend.position = c(0.8,0.85)) +
+  labs(title="distance to neasest TSS for all TSSs")
+ggsave("TSSs_distance2nearest_TSS_density.pdf")
+ggsave("TSSs_distance2nearest_TSS_density.png")
+
+
+ggplot(distance, aes(y=log10(distance+0.1), x=type, fill=type)) + 
+  geom_boxplot(width=0.5) +
+  scale_fill_manual(values = c("#00bfc4","#f8766d")) +
+  theme_minimal() +
+  geom_hline(yintercept = log10(50), col="grey",linetype="dashed", size=1) +
+  theme(title = element_text(face="bold",size=12)) +
+  labs(title="distance to neasest TSS for all TSSs")
+
+ggsave("TSSs_distance2nearest_TSS_boxplot.pdf")
+ggsave("TSSs_distance2nearest_TSS_boxplot.png")
+
+ggplot(distance, aes(y=distance, x=type, fill=type)) + 
+  geom_boxplot(width=0.5, outlier.size = 0.1) +
+  scale_fill_manual(values = c("#00bfc4","#f8766d")) +
+  theme_minimal() + coord_cartesian(ylim = c(0, 150)) +
+  geom_hline(yintercept = 50, col="grey",linetype="dashed", size=1) +
+  theme(title = element_text(face="bold",size=12)) +
+  labs(title="distance to neasest TSS for all TSSs")
+
+ggsave("TSSs_distance2nearest_TSS_boxplot_zoomin.pdf")
+ggsave("TSSs_distance2nearest_TSS_boxplot_zoomin.png")
+
+#coord_cartesian(ylim = c(0, 500)) +
+
+####################
+# TSS nearest TSS distance (same gene)
+####################
+distance_gene = read.table("TSS_nearest_TSS_distance_sameGene_annotated.bed")
+colnames(distance_gene) = c("gene","tss","distance", "type")
+
+
+ggplot(distance_gene, aes(log10(distance+0.1), fill=type, col=type)) + 
+  geom_density(size=1, alpha=0.5) +
+  scale_fill_manual(values = c("#00bfc4","#f8766d")) +
+  scale_color_manual(values = c("#00bfc4","#f8766d")) +
+  theme_minimal() +
+  geom_vline(xintercept = log10(50), col="grey",linetype="dashed", size=1) +
+  theme(title = element_text(face="bold",size=12),
+        legend.position = c(0.8,0.85)) +
+  labs(title="distance to neasest TSS for all TSSs\n(same gene)")
+ggsave("TSSs_distance2nearest_TSS_density_sameGene.pdf")
+ggsave("TSSs_distance2nearest_TSS_density_sameGene.png")
+
+ggplot(distance_gene, aes(y=log10(distance+0.1), x=type, fill=type)) + 
+  geom_boxplot(width=0.5) +
+  scale_fill_manual(values = c("#00bfc4","#f8766d")) +
+  theme_minimal() +
+  geom_hline(yintercept = log10(50), col="grey",linetype="dashed", size=1) +
+  theme(title = element_text(face="bold",size=12)) +
+  labs(title="distance to neasest TSS for all TSSs\n(same gene)")
+
+ggsave("TSSs_distance2nearest_TSS_boxplot_sameGene.pdf")
+ggsave("TSSs_distance2nearest_TSS_boxplot_sameGene.png")
+
+ggplot(distance_gene, aes(y=distance, x=type, fill=type)) + 
+  geom_boxplot(width=0.5, outlier.size = 0.1) +
+  scale_fill_manual(values = c("#00bfc4","#f8766d")) +
+  theme_minimal() + coord_cartesian(ylim = c(0, 150)) +
+  geom_hline(yintercept = 50, col="grey",linetype="dashed", size=1) +
+  theme(title = element_text(face="bold",size=12)) +
+  labs(title="distance to neasest TSS for all TSSs\n(same gene)")
+
+ggsave("TSSs_distance2nearest_TSS_boxplot_sameGene_zoomin.pdf")
+ggsave("TSSs_distance2nearest_TSS_boxplot_sameGene_zoomin.png")
 
 
 ####################
+
+
 
